4caa101ffeae08641ef20c4ae6536c737d1be1af
Thomas Perryman <tcw.perryman@gmail.com>
2018-05-23 19:03:14 +0100

    ran stats method from CLI

--- src/databasic/Main.java
+++ src/databasic/Main.java
@@ -1,10 +1,13 @@
 package databasic;
 
-public class Main {
-    private Cli cli = new Cli();
+public class Main{
+
 
     public static void main(String[] args){
         System.out.println("Welcome to databasic!");
-
+        Cli cli = new Cli();
+        if (args[0].equals("stats")){
+            cli.stats();
+        }
     }
 }
