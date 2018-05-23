162f9c210eb60aeb310cb56d989ba7dd18daebc4
Thomas Perryman <tcw.perryman@gmail.com>
2018-05-23 19:48:35 +0100

    added sys outs

--- src/databasic/Formatter.java
+++ src/databasic/Formatter.java
@@ -2,9 +2,11 @@ package databasic;
 
 class Formatter {
     String formatQuery(Object object){
+        System.out.println("Formatter.formatQuery");
         return null;
     }
     String formatInsert(Id id){
+        System.out.println("Formatter.formatInsert");
         return null;
     }
 
